264a931aeb929b92bdd9ad6292bf7ea73a4afb1e
tkir <kirill.titenko@gmail.com>
2017-11-09 10:12:53 +0200

    api bugs fixed

--- api.php
+++ api.php
@@ -2,19 +2,39 @@
 
 class BC_Creator_API
 {
-    public static function post($path, $data){
-        $config = json_decode(file_get_contents(__DIR__ . "/config.json"));
-        $url = $config->api->endpoint . $path;
+//    public static function post($path, $data){
+//        $config = json_decode(file_get_contents(__DIR__ . "/config.json"));
+//        $url = $config->api->endpoint . $path;
+//
+//        $options = array(
+//            'http' => array(
+//                'header'  => "Content-type: application/json
+//                              Accept: application/json\r\n",
+//                'method'  => 'POST',
+//                'content' => $data
+//            )
+//        );
+//        $context  = stream_context_create($options);
+//        return file_get_contents($url, false, $context);
+//    }
 
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/json
-                              Accept: application/json\r\n",
-                'method'  => 'POST',
-                'content' => $data
-            )
-        );
-        $context  = stream_context_create($options);
-        return file_get_contents($url, false, $context);
+    public static function post($path, $data){
+	    $config = json_decode(file_get_contents(__DIR__ . "/config.json"));
+	    $url = $config->api->endpoint . $path;
+
+	    $args = array(
+		    'method' => 'POST',
+		    'timeout' => 45,
+		    'limit_response_size' => 30000000,
+		    'blocking' => true,
+		    'headers' => array(
+		    	"Content-type"=>"application/json",
+			    "Accept"=>"application/json"
+		    ),
+		    'body' => $data
+	    );
+	    $response = wp_remote_post( $url, $args );
+	    file_put_contents(__DIR__.'/test.json', $response['body']);
+	    return $response['body'];
     }
 }
