f9f8b4d08f2875444021aa34cef992e5ab01a293
tkir <kirill.titenko@gmail.com>
2017-11-21 11:09:37 +0200

    creator toggle + tabs

--- row/src/app/side-switch/side-switch.component.ts
+++ row/src/app/side-switch/side-switch.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterContentInit, Component, ElementRef} from '@angular/core';
 import {OptionsService} from "../services/options.service";
 import {DataService} from "../services/data.service";
 
@@ -7,20 +7,30 @@ import {DataService} from "../services/data.service";
   templateUrl: './side-switch.component.html',
   styleUrls: ['./side-switch.component.css']
 })
-export class SideSwitchComponent {
+export class SideSwitchComponent implements AfterContentInit {
+  private buttons: Element[];
 
-  constructor(public options: OptionsService,
+  ngAfterContentInit(): void {
+    this.buttons = this.el.nativeElement.querySelectorAll('button');
+    this.buttons[0].classList.add('active');
+  }
+
+  constructor(private el: ElementRef,
+              public options: OptionsService,
               private dataService: DataService) {
   }
 
   private currentSide: string;
 
-  changeSide(side) {
+  changeSide(event, side) {
     this.dataService.changeSide(side);
+    this.buttons.forEach(el => (el == event.target) ? el.classList.add('active') : el.classList.remove('active'));
   }
 
-  setSideNum(sideNum) {
-    this.dataService.changeSideNumber(sideNum);
+  setSideNum() {
+    this.dataService.changeSideNumber((this.options.isDoubleSide)?'single':'double');
+    this.buttons.forEach(el => el.classList.remove('active'));
+    this.buttons[0].classList.add('active');
   }
 
 }
