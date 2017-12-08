955cec5162a45b76b6b4c47f1b3ea42529d12a9f
tkir <kirill.titenko@gmail.com>
2017-12-08 18:09:28 +0200

    undo-redo start

---
+++ row/src/app/services/undo-redo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UndoRedoService {
+
+  constructor() { }
+
+  public deleteItem(item){
+
+  }
+
+  public textChange(before:string, curr:string, item){
+
+  }
+
+  public itemPositionChange(prev, curr, item){
+
+  }
+
+}
