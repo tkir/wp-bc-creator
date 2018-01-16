16c5593054ee93d36c5ddca2edb8e6b2fb4c35cf
tkir <kirill.titenko@gmail.com>
2018-01-16 23:50:55 +0200

    front-back bug fixed

--- row/src/app/front-back/front-back.component.ts
+++ row/src/app/front-back/front-back.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component, ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
 import {OptionsService} from "../services/options.service";
 import {DataService} from "../services/data.service";
 
@@ -7,22 +7,14 @@ import {DataService} from "../services/data.service";
   templateUrl: './front-back.component.html',
   styleUrls: ['./front-back.component.css', '../css/tabs.css']
 })
-export class FrontBackComponent implements AfterContentInit {
-  private buttons: Element[];
+export class FrontBackComponent {
 
-  ngAfterContentInit(): void {
-    this.buttons = this.el.nativeElement.querySelectorAll('button');
-    (this.dataService.side == 'front') ? this.buttons[0].classList.add('active') : this.buttons[1].classList.add('active');
-  }
-
-  constructor(private el: ElementRef,
-              public options: OptionsService,
-              private dataService: DataService) {
+  constructor(public options: OptionsService,
+              public dataService: DataService) {
   }
 
   changeTab(event, side) {
     this.dataService.changeSide(side);
-    this.buttons.forEach(el => (el == event.target) ? el.classList.add('active') : el.classList.remove('active'));
   }
 
 }
--- row/src/app/side-switch/side-switch.component.ts
+++ row/src/app/side-switch/side-switch.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component, ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
 import {OptionsService} from "../services/options.service";
 import {DataService} from "../services/data.service";
 
@@ -7,30 +7,14 @@ import {DataService} from "../services/data.service";
   templateUrl: './side-switch.component.html',
   styleUrls: ['./side-switch.component.css', '../css/tabs.css']
 })
-export class SideSwitchComponent implements AfterContentInit {
-  private buttons: Element[];
+export class SideSwitchComponent {
 
-  ngAfterContentInit(): void {
-    this.buttons = this.el.nativeElement.querySelectorAll('button');
-    (this.dataService.side == 'front') ? this.buttons[0].classList.add('active') : this.buttons[1].classList.add('active');
-  }
-
-  constructor(private el: ElementRef,
-              public options: OptionsService,
+  constructor(public options: OptionsService,
               private dataService: DataService) {
   }
 
-  private currentSide: string;
-
-  // changeTab(event, side) {
-  //   this.dataService.changeSide(side);
-  //   this.buttons.forEach(el => (el == event.target) ? el.classList.add('active') : el.classList.remove('active'));
-  // }
-
   setSideNum() {
     this.dataService.changeSideNumber((this.options.isDoubleSide) ? 'single' : 'double');
-    this.buttons.forEach(el => el.classList.remove('active'));
-    this.buttons[0].classList.add('active');
   }
 
 }
